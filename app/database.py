89b4e1dfd8bde7867dbe218f927e2ffea97dbf47
ArjunTrushenPatel <arjun_ragu@outlook.com>
2025-12-27 14:08:52 -0600

    Include database password in connection URL

--- app/database.py
+++ app/database.py
@@ -5,7 +5,11 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.database_username}:{settings.database_password}"
+    f"@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+)
+
 # SQLALCHEMY_DATABASE_URL = 'postgresql://arjunragu:@localhost/fastapi'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
