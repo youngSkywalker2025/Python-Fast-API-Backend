6b7176481085b1faee75491557883468ab70df11
ArjunTrushenPatel <arjun_ragu@outlook.com>
2025-12-28 08:13:19 -0600

    hello world

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI To-Do API!"}
+    return {"message": "Hello from FastAPI To-Do API!!!!"}
 
 
 
